38b12a630a077b4c18bc63cdf21367d1271fc161
gs298 <shrey.om11@gmail.com>
2015-12-27 23:52:29 -0800

    commit

--- BankJava/src/businesslogic/AccountObject.java
+++ BankJava/src/businesslogic/AccountObject.java
@@ -2,6 +2,7 @@ package businesslogic;
 import java.util.List;
 public class AccountObject extends DataObject{
 // Account details are added in this class.
+	private int x;
 private double balance;
 private int acctNumber;
 private String last_trans_date;
